2bc33874f8829272c9eb514fc11b16b6200b1872
ilyassoussou <ilyassamazouz022@gmail.com>
2024-07-28 14:21:27 +0100

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,58 @@
+//creating an array that contains questions, options and answers
+let questions = [
+    {
+        numb: 1,
+        question: "Which car manufacturer is known for producing the Mustang?",
+        answer: "Ford",
+        options: [
+            "Chevrolet",
+            "Ford",
+            "Toyota",
+            "Honda",
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which country is the automaker BMW headquartered in?",
+        answer: "Germany",
+        options: [
+            "Germany",
+            "Italy",
+            "US",
+            "Japan",
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which component of a car converts the engine's power into motion?",
+        answer: "Transmission",
+        options: [
+            "Radiator",
+            "Transmission",
+            "Carburetor",
+            "Alternator",
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which gas is commonly used as fuel in diesel engines?",
+        answer: "Diesel",
+        options: [
+            "Propane",
+            "Gasoline",
+            "Diesel",
+            "Natural Gas",
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which term refers to a vehicle's ability to maintain grip on the road surface?",
+        answer: "Traction",
+        options: [
+            "Traction",
+            "Acceleration",
+            "Torque",
+            "Compression",
+        ]
+    },
+]
